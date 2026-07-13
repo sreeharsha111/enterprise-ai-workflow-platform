4c3cf29ca0054806f164eb6e0b11899db0b4099d
sreeharsha111 <dsreeharsha143@gmail.com>
2026-07-13 23:13:38 +0530

    added the db connection and impelemted the organisation with postgres and create data tables  using alembic

--- backend/app/models/organization.py
+++ backend/app/models/organization.py
@@ -1,16 +1,67 @@
 from dataclasses import dataclass
-from uuid import UUID
+from datetime import datetime
+from sqlalchemy import Column, DateTime, Enum, Integer, String
+from app.core.database import Base
+# from uuid import UUID
 
 from app.enums.status import Status
-from app.models.base_entity import BaseEntity
+# from app.models.base_entity import BaseEntity
 
 
 @dataclass
-class Organization(BaseEntity):
-    id: UUID
-    organization_code: str
-    organization_name: str
-    company_email: str
-    employee_size: str
-    timezone: str
-    status: Status
+class Organization(Base):
+    __tablename__ ="organizations"
+    id = Column(String(36), primary_key=True, nullable=False)
+
+    organization_code = Column(
+        String(20),
+        unique=True,
+        nullable=False
+    )
+
+    organization_name = Column(
+        String(255),
+        nullable=False
+    )
+
+    company_email = Column(
+        String(255),
+        unique=True,
+        nullable=False
+    )
+
+    employee_size = Column(
+        Integer,
+        nullable=False
+    )
+
+    timezone = Column(
+        String(100),
+        nullable=False
+    )
+
+    status = Column(
+        Enum(Status),
+        nullable=False
+    )
+
+    created_at = Column(
+        DateTime,
+        default=datetime.utcnow,
+        nullable=False
+    )
+
+    updated_at = Column(
+        DateTime,
+        nullable=True
+    )
+
+    created_by = Column(
+        String(36),
+        nullable=True
+    )
+
+    updated_by = Column(
+        String(36),
+        nullable=True
+    )
